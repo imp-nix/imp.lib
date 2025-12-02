07dda4e159e6635528633dd9f487a771e52338a9
Albert O'Shea <albertoshea2@gmail.com>
2025-12-02 15:06:53 +1100

    fix: self-referential attrs

--- flake.nix
+++ flake.nix
@@ -23,11 +23,23 @@
     in
     {
       tests = import ./tests { inherit lib; };
+
+      # Export imp as a callable functor with essential methods
+      # Avoid exporting self-referential attrs (leafs, filter, map, etc.)
+      __functor = imp.__functor;
+      __config = imp.__config; # Required for __functor to work
+      withLib = imp.withLib;
+      addPath = imp.addPath;
+      addAPI = imp.addAPI;
+      new = imp.new;
+
+      # High-level tree operations (require withLib first)
+      tree = imp.tree;
+      treeWith = imp.treeWith;
+      configTree = imp.configTree;
+      configTreeWith = imp.configTreeWith;
+      flakeOutputs = imp.flakeOutputs;
     }
-    // removeAttrs imp [
-      "files"
-      "result"
-    ]
     // flake-utils.lib.eachDefaultSystem (
       system:
       let
